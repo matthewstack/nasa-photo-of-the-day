ff7e67b73d7607c265e4b3e62a3bf939ad24d7f6
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-09-09 21:10:29 -0400

    added in another component + tailwind css styling

--- src/Photo/Photo.js
+++ src/Photo/Photo.js
@@ -1,11 +1,29 @@
 import React from "react";
-import PhotoDetails from "./PhotoDetails";
+
 const Photo = (props) => {
   const { photo } = props;
   return (
-    <div class="photoContainer">
-      <img src={photo.url}></img>;
-      <PhotoDetails photo={photo} />
+    <div className="bg-white">
+      <section aria-labelledby="features-heading" className="relative">
+        <div className="aspect-w-3 aspect-h-2 overflow-hidden sm:aspect-w-5 lg:aspect-none lg:absolute lg:w-1/2 lg:h-full lg:pr-4 xl:pr-16">
+          <img
+            src={photo.url}
+            className="h-full w-full object-center object-cover lg:h-full lg:w-full"
+          />
+        </div>
+
+        <div className="max-w-2xl mx-auto pt-16 pb-24 px-4 sm:pb-32 sm:px-6 lg:max-w-7xl lg:pt-32 lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+          <div className="lg:col-start-2">
+            <h2 id="features-heading" className="font-medium text-gray-500">
+              {photo.date}
+            </h2>
+            <p className="mt-4 text-4xl font-extrabold text-gray-900 tracking-tight">
+              {photo.title}
+            </p>
+            <p className="mt-4 text-gray-500">{photo.explanation}</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
